1839137114b89995159094be89e2e6bacd5824f7
yegresvonason <yegresvonason@gmail.com>
2018-07-08 21:21:14 +0300

    add text components

--- src/App.js
+++ src/App.js
@@ -2,32 +2,56 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Grid from 'react-bootstrap/lib/Button';
-import Row from 'react-bootstrap/lib/Button';
-import Col from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Grid';
 
 import styled from 'styled-components';
 
+import Header from './components/Header';
+import Menu from './components/Menu';
+import Main from './components/Main';
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 91px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 100px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="wrapper">
-        <SayFullName name="Sergey" surname="Nosanov" link="https://google.com" />
-        <SayFullName name="Mike" surname="Shinoda" link="https://google.com" />
-        <SayFullName name="Rob" surname="Bourdon" link="https://google.com" />
-      </div>
-    );
-  }
-}
-
-function SayFullName(props) {
-  return (
-      <div>
-      <h1> Мое имя: {props.name}, фамилия: {props.surname} </h1>
-      <a href={props.link}>Ссылка на мой профиль</a>
+      <div className = "App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+          </Grid>
+        </HeaderWrapper>
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
       </div>
     )
+  }
 }
 
 export default App;
